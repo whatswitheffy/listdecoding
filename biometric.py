197b75ba6d9c7d7524355b3147399221a020c4d2
whatswitheffy <48794170+whatswitheffy@users.noreply.github.com>
2023-04-27 01:05:31 +0600

    Update biometric.py

--- biometric.py
+++ biometric.py
@@ -1,34 +1,50 @@
 from reedMuller import FirstOrderReedMuller
 from tensorProductCode import TensorProductCode
-from utils import seqGenerator, generateBitErrors
+from utils import generateRandomBitVec, generateBitErrors
 import hashlib
 import numpy as np
 
 
 class BiometricSystem:
-    def __init__(self, lenOfIris, lenOfKey):
-        self.irisCode, self.key = seqGenerator(lenOfIris, lenOfKey)
-        self.codeProduct = TensorProductCode(FirstOrderReedMuller(2), FirstOrderReedMuller(2))
-        self.encodedKey = self.codeProduct.encode(self.key)
-        self.hash = self.keyHash()
-        self.lock = self.irisAndCodewordXOR()
+    def __init__(self, code):
+        self.code = code
+        self.hashFunction = hashlib.sha3_512()
 
-    def keyHash(self):
-        hash = hashlib.sha3_512()
-        hash.update(self.key)
-        print("Hash of key:", hash.hexdigest())
-        return hash.hexdigest()
-    
-    def irisAndCodewordXOR(self):
-        lock = np.logical_xor(self.irisCode, self.encodedKey)
-        print("LOCK: ", lock.astype(int) )
-        return lock
-            
+    def enrollment(self, irisCode):
+        key = generateRandomBitVec(self.code.k)
+        encodedKey = self.code.encode(key)
+        print("KEY CODEWORD: ", encodedKey)
+        self.hashFunction.update(encodedKey)
+
+        self.keyHash = self.hashFunction.hexdigest()
+        print("KEY CODEWORD HASH: ", self.keyHash)
+        self.lock = np.logical_xor(encodedKey, irisCode)
+
+    def authentication(self, irisCode):
+        unlockedKey = np.logical_xor(self.lock, irisCode)
+        decodedList = self.code.listDecode(unlockedKey, eps=0.001)
+
+        for codeword in decodedList:
+            print(codeword)
+            self.hashFunction.update(codeword)
+            codewordHash = self.hashFunction.hexdigest()
+            print(codewordHash)
+            if self.keyHash == codewordHash:
+                return True
+        
+        return False
+
 
 if __name__ == "__main__":
-    biom = BiometricSystem(16, 9)
-    # decoded_list = rm.listDecode(biom.codewordWithErrors, 0.1)
-    # for decoded_codeword in decoded_list:
-    #     print(decoded_codeword)
-    #     if np.allclose(decoded_codeword, biom.codeword):
-    #         print("EQUAL")
+    system = BiometricSystem(TensorProductCode(FirstOrderReedMuller(6), FirstOrderReedMuller(5)))
+
+    trueIrisCode = generateRandomBitVec(2048)
+    print("TRUE IRIS CODE:", trueIrisCode)
+    system.enrollment(trueIrisCode)
+
+    trueIrisCodeWithErrors = generateBitErrors(trueIrisCode, 50)
+    falseIrisCode = generateRandomBitVec(2048)
+
+    print("TRUE IRIS CODE AUTH RESULT:", system.authentication(trueIrisCode))
+    # print("TRUE IRIS WITH ERRORS AUTH RESULT:", system.authentication(trueIrisCodeWithErrors))
+    # print("FALSE IRIS CODE AUTH RESULT:", system.authentication(falseIrisCode))
